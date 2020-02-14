413c456c2ad18468adb2d2a7b69f891fd567b4e2
Andrija Vucinic <andrija.vucinic@gmail.com>
2020-02-14 17:34:32 +0100

    comment

--- concat-css.php
+++ concat-css.php
@@ -37,6 +37,7 @@ class Page_Optimize_CSS_Concat extends WP_Styles {
 		$stylesheet_group_index = 0;
 		// Merge CSS into a single file
 		$concat_group = 'concat';
+		// Concat group on top (first array element gets processed earlier)
 		$stylesheets[ $concat_group ] = array();
 
 		foreach ( $this->to_do as $key => $handle ) {
