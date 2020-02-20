f360717e5a57f9a06aa63422ec6cf8c3d088b8f2
Brandon Payton <brandon@happycode.net>
2020-02-19 22:04:47 -0700

    Add some files for testing path resolution

---
+++ tests/test_URL_to_file_mapping.php
@@ -0,0 +1,27 @@
+<?php
+
+class Test_URL_To_File_Mapping extends PHPUnit\Framework\TestCase {
+	// TODO: URL without host is based on ABSPATH
+	// TODO: Separate plugin URL with site URL host. Exist/non-exist
+	// TODO: Separate content URL with site URL host. Exist/non-exist
+	// TODO: Site root URL. Exist/non-exist
+	// TODO: Plugin URL under content URL dir but separate from site URL. Same host as site URL.
+	// TODO: Plugin URL under content URL which is under site URL. Same host as site URL.
+	// TODO: Content URL descended from site URL
+	// TODO: Plugin URL with different host than site URL
+	// TODO: Content URL with different host than site URL
+	// TODO: Relative URLs
+}
+
+class Test_URI_Path_To_File_Mapping extends PHPUnit\Framework\TestCase {
+	// TODO: Separate plugin path when plugin URL has same host as site URL. Exist/non-exist
+	// TODO: Separate content path when plugin URL has same host as site URL. Exist/non-exist
+	// TODO: Separate content path content URL with site URL host. Exist/non-exist
+	// TODO: Site root path. Exist/non-exist
+	// TODO: Plugin path under content path dir but separate from site path. Same host as site URL.
+	// TODO: Plugin path under content path which is under site path. Same host as site URL.
+	// TODO: Content path descended from site path
+	// TODO: Plugin URL with different host than site URL
+	// TODO: Content URL with different host than site URL
+	// TODO: Relative URLs
+}
