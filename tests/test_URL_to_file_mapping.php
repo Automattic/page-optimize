6fc2ab59ae13515a5cc4d0f186b1af31a71e8385
Brandon Payton <brandon@happycode.net>
2020-03-05 19:29:41 -0700

    Add some test data and a WIP test

--- tests/test_URL_to_file_mapping.php
+++ tests/test_URL_to_file_mapping.php
@@ -1,5 +1,7 @@
 <?php
 
+require_once __DIR__ . '/../dependency-path-mapping.php';
+
 class Test_URL_To_File_Mapping extends PHPUnit\Framework\TestCase {
 	// TODO: URL without host is based on ABSPATH
 	// TODO: Separate plugin URL with site URL host. Exist/non-exist
@@ -14,12 +16,46 @@ class Test_URL_To_File_Mapping extends PHPUnit\Framework\TestCase {
 }
 
 class Test_URI_Path_To_File_Mapping extends PHPUnit\Framework\TestCase {
+
+
+	// $site_url = null, // default site URL is determined dynamically
+	// $site_dir = ABSPATH,
+	// $plugin_url = WP_PLUGIN_URL,
+	// $plugin_dir = WP_PLUGIN_DIR,
+	// $content_url = WP_CONTENT_URL,
+	// $content_dir = WP_CONTENT_DIR
+	// TODO: Test URI and FS paths with and without trailing slashes
+
+	// TODO: Plugin path under content path which is under site path. Same host as site URL.
+	function test_nested_paths() {
+		$site_url = 'http://example.com';
+		$site_dir = __DIR__ . '/data/url-to-file-mapping/site';
+		$content_url = "$site_url/wp-content";
+		$content_dir = "$site_dir/content";
+		$plugin_url = "$content_url/plugins";
+		$plugin_dir = "$content_dir/plugins";
+
+		$dpm = new Page_Optimize_Dependency_Path_Mapping(
+			$site_url,
+			$site_dir,
+			$content_url,
+			$content_dir,
+			$plugin_url,
+			$plugin_dir
+		);
+
+		//
+		// WIP
+		//
+		$this->assertFalse( $dpm->uri_path_to_fs_path( '/nonexistent' ) );
+	}
+
 	// TODO: Separate plugin path when plugin URL has same host as site URL. Exist/non-exist
+
 	// TODO: Separate content path when plugin URL has same host as site URL. Exist/non-exist
 	// TODO: Separate content path content URL with site URL host. Exist/non-exist
 	// TODO: Site root path. Exist/non-exist
 	// TODO: Plugin path under content path dir but separate from site path. Same host as site URL.
-	// TODO: Plugin path under content path which is under site path. Same host as site URL.
 	// TODO: Content path descended from site path
 	// TODO: Plugin URL with different host than site URL
 	// TODO: Content URL with different host than site URL
