b744fed0023b6cf64e638a8e22b831781a658a55
Brandon Payton <brandon@happycode.net>
2021-06-29 14:45:03 -0400

    Test local request

--- tests/test_output.php
+++ tests/test_output.php
@@ -5,5 +5,7 @@ require_once __DIR__ . '/../dependency-path-mapping.php';
 class Test_Output extends PHPUnit\Framework\TestCase {
 	function test_concat_output() {
 		var_dump( $_SERVER );
+		echo "Testing HTTP request...\n";
+		var_export( wp_remote_request( 'http://127.0.0.1/' ), true );
 	}
 }
