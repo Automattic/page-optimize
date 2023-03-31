8f09151bb99851f6b8ebeec73c0ff46b8092156e
Brandon Payton <brandon@happycode.net>
2023-03-30 20:53:53 -0400

    Bump version to v0.5.3

--- page-optimize.php
+++ page-optimize.php
@@ -4,7 +4,7 @@ Plugin Name: Page Optimize
 Plugin URI: https://wordpress.org/plugins/page-optimize/
 Description: Optimizes JS and CSS for faster page load and render in the browser.
 Author: Automattic
-Version: 0.5.2
+Version: 0.5.3
 Author URI: http://automattic.com/
 */
 
